be8213534aefd15f68be118353602889e99877d0
AkimJemi <rlawoals623@gmail.com>
2024-08-19 21:51:40 +0900

    20240819 JPA init

---
+++ src/main/java/com/pro/spring/JpaApplication.java
@@ -0,0 +1,45 @@
+package com.pro.spring;
+
+import java.time.LocalDateTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.pro.spring.entity.UserEntity;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+@SpringBootApplication
+public class JpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApplication.class, args);
+
+        EntityManagerFactory entityManagerFactory = Persistence
+                .createEntityManagerFactory("basicjpa");
+        EntityManager entManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction entityTransaction = entManager.getTransaction();
+
+        try {
+            entityTransaction.begin();
+
+            UserEntity userEntity = new UserEntity("1", "2", LocalDateTime.now(),
+                    LocalDateTime.now());
+            entManager.persist(userEntity);
+            entityTransaction.commit();
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+            entityTransaction.rollback();
+        } finally {
+            entManager.close();
+        }
+        entityManagerFactory.close();
+
+    }
+
+}
---
+++ src/main/java/com/pro/spring/entity/UserEntity.java
@@ -0,0 +1,36 @@
+package com.pro.spring.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class UserEntity {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public UserEntity(String id, String name, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public UserEntity() {
+        super();
+    }
+
+}
